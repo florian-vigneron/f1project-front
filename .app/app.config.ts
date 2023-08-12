f4402c9d04eff5e64259026670175ec8fc2ba099
Florian Vigneron <florian.vigneron11@gmail.com>
2023-08-12 18:58:36 +0200

    first commit

---
+++ .app/app.config.ts
@@ -0,0 +1,26 @@
+/**
+ * This file is used to configure the app
+ *
+ * If you have the "Cannot find name 'defineAppConfig'.ts(2304)" error
+ * update the root tsconfig.json file to include the following:
+ *
+ *  "extends": "./.app/.nuxt/tsconfig.json"
+ *
+ */
+
+export default defineAppConfig({
+  tairo: {
+    sidebar: {
+      navigation: {
+        enabled: true,
+        startOpen: true,
+        logo: {
+          // sidebar logo configuration
+        },
+        items: [
+          // navigation items configuration
+        ],
+      },
+    },
+  },
+})
---
+++ .app/pages/index.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+// set compile time meta information
+definePageMeta({
+  // static meta information can be added to vue-router, we use it
+  // to generate the search index in the demo
+  title: 'My first page',
+})
+
+// meta information can also be added to the head
+useHead({
+  meta: [{ name: 'description', content: 'My amazing site.' }],
+  bodyAttrs: {
+    class: 'test',
+  },
+  script: [{ innerHTML: "console.log('Hello world')" }],
+})
+
+// Here you can define your page logic
+const accordion = ref([
+  {
+    title: 'Accordion Item 1',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quo tandem modo? Inde igitur, inquit, ordiendum est. Primum quid tu dicis breve? Duo Reges: constructio interrete.',
+  },
+  {
+    title: 'Accordion Item 2',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quo tandem modo? Inde igitur, inquit, ordiendum est. Primum quid tu dicis breve? Duo Reges: constructio interrete.',
+  },
+  {
+    title: 'Accordion Item 3',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quo tandem modo? Inde igitur, inquit, ordiendum est. Primum quid tu dicis breve? Duo Reges: constructio interrete.',
+  },
+])
+</script>
+
+<template>
+  <div>
+    <!-- The page content goes here -->
+    <div>
+      <BaseAccordion :items="accordion" />
+    </div>
+  </div>
+</template>
