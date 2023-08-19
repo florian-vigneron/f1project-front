b83b7fb8eb34232a6296f816e7e313d1cb361cc1
Florian Vigneron <florian.vigneron11@gmail.com>
2023-08-19 23:12:17 +0200

    Okay

--- .app/pages/formule1/drivers.vue
+++ .app/pages/formule1/drivers.vue
@@ -29,44 +29,53 @@ useHead({
 </script>
 
 <template>
-  <div class="flex flex-col gap-4">
-    <BaseBreadcrumb :items="breadcrumb">
-      <Icon name="lucide:chevron-right" class="block h-4 w-4" />
-    </BaseBreadcrumb>
-  </div>
-  <div class="relative">
-    <div class="ltablet:col-span-6 col-span-12 lg:col-span-6">
-      <div class="flex h-full flex-col">
-        <div class="grid h-full grid-cols-12 gap-6">
-          <div :key="0" class="col-span-6 sm:col-span-3">
-            <NuxtLink to="/formule1/driver">
-              <div class="group flex h-full flex-col">
+  <div>
+    <div class="flex flex-col gap-4">
+      <BaseBreadcrumb :items="breadcrumb">
+        <Icon name="lucide:chevron-right" class="block h-4 w-4" />
+      </BaseBreadcrumb>
+    </div>
+    <TairoContentWrapper>
+      <div
+        class="ltablet:grid-cols-4 grid w-full gap-4 sm:grid-cols-2 lg:grid-cols-4"
+      >
+        <TransitionGroup
+          enter-active-class="transform-gpu"
+          enter-from-class="opacity-0 -translate-x-full"
+          enter-to-class="opacity-100 translate-x-0"
+          leave-active-class="absolute transform-gpu"
+          leave-from-class="opacity-100 translate-x-0"
+          leave-to-class="opacity-0 -translate-x-full"
+        >
+        </TransitionGroup>
+        <div :key="0" group p-4>
+          <NuxtLink to="/formule1/driver">
+            <div class="group flex h-full flex-col">
+              <div
+                class="bg-muted-200 dark:bg-muted-800 relative h-full w-full overflow-hidden rounded-2xl"
+              >
+                <img
+                  src="/img/illustrations/project.jpg"
+                  alt="video.title"
+                  class="w-full object-cover object-center"
+                />
                 <div
-                  class="bg-muted-200 dark:bg-muted-800 relative h-full w-full overflow-hidden rounded-2xl"
+                  class="absolute bottom-0 px-4 py-3 bg-primary-500/20 w-full"
                 >
-                  <img
-                    src="/img/illustrations/project.jpg"
-                    alt="video.title"
-                    class="w-full object-cover object-center"
-                  />
-                  <div
-                    class="absolute bottom-0 px-4 py-3 bg-primary-500/20 w-full"
-                  >
-                    <h1 class="text-white font-semibold text-2xl">
-                      <span
-                        class="text-white font-semibold text-5xl w-48 text-center"
-                        >#1</span
-                      >
-                      Max Verstappen
-                    </h1>
-                  </div>
+                  <h1 class="text-white font-semibold text-2xl">
+                    <span
+                      class="text-white font-semibold text-5xl w-48 text-center"
+                      >#1</span
+                    >
+                    Max Verstappen
+                  </h1>
                 </div>
               </div>
-            </NuxtLink>
-          </div>
+            </div>
+          </NuxtLink>
         </div>
       </div>
-    </div>
+    </TairoContentWrapper>
   </div>
 </template>
 
